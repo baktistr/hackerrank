51bb39eb221165b1317d4955a8e045a983ec77ce
unknown <bakti.satria.a@gmail.com>
2020-11-12 13:32:20 +0700

    day1 quartiles

---
+++ 10 Days of Statistics/Day 1/quartiles.py
@@ -0,0 +1,24 @@
+# https://www.hackerrank.com/challenges/s10-quartiles/problem
+
+def median(size, data):
+    if (size % 2 == 0):
+        return int((data[int(size/2)-1]+data[int(size/2)])/2)
+    else:
+        return int(data[int(size/2)])
+
+size = int(input())
+numbers = sorted(list(map(int, input().split())))
+
+if (size % 2 == 0):
+    data_low = numbers[0:int(size/2)]
+    data_high = numbers[int(size/2):]
+else:
+    data_low = numbers[0:int(size/2)]
+    data_high = numbers[int(size/2)+1:]
+
+print(median(len(data_low), data_low))
+print(median(size, numbers))
+print(median(len(data_high), data_high))
+#print(numbers)
+#print(data_low)
+#print(data_high)
